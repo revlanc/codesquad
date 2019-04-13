c143d2b1a8d5e8f5218e510e7422c40f80c13e8a
allen <revenoir7@gmail.com>
2019-04-13 10:24:03 +0900

    테스트 케이스 배치 수정

--- step3/myReduce.js
+++ step3/myReduce.js
@@ -13,13 +13,15 @@ const myReduce = (arr, callback, initialValue) => {
 }
 
 const arr1 = [1, 2, 3, 4, 5]
-const arr2 = [[1, 2], [3, 4], [5, 6]]
-const arr3 = [{ x: 1 }, { x: 2 }, { x: 3 }]
-const arr4 = [{ Kim: 90 }, { Lee: 95 }, { Park: 85 }]
-
 console.log(myReduce(arr1, (acc, cur) => acc + cur))
 console.log(myReduce(arr1, (acc, cur) => acc * cur))
 console.log(myReduce(arr1, function (acc, cur) { if (cur % 2 !== 0) { acc.push(cur * 2) } return acc }, []))
+
+const arr2 = [[1, 2], [3, 4], [5, 6]]
 console.log(myReduce(arr2, (acc, cur) => acc.concat(cur), []))
+
+const arr3 = [{ x: 1 }, { x: 2 }, { x: 3 }]
 console.log(myReduce(arr3, (acc, cur) => acc + cur.x, 0))
+
+const arr4 = [{ Kim: 90 }, { Lee: 95 }, { Park: 85 }]
 console.log(myReduce(arr4, function (acc, cur) { for (let key in cur) { if (cur[key] >= 90) { acc.push(key) } } return acc }, []))
