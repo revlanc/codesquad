d58181055cbe58979d8e7db0f357236ef5b0b51a
allen <revenoir7@gmail.com>
2019-04-19 11:20:35 +0900

    feat: Create todo.js
    - Step4-2 할일관리 프로그램 초기설계

---
+++ step4/step4-2/todo.js
@@ -0,0 +1,29 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const generateID = () => {
+
+}
+
+const todos = {
+    todolists =[]
+}
+
+todos.show = () => {
+
+}
+
+todos.add = () => {
+
+}
+
+todos.delete = () => {
+
+}
+
+todos.update = () => {
+
+}
