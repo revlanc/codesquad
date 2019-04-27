b1da0aa23f8a0ff19d3819ede70baccae091fefe
allen <revenoir7@gmail.com>
2019-04-27 22:38:31 +0900

    refactor: 결과출력 메시지를 객체매핑하여 한개의 메서드로 수정

--- step5/step5-3/controller.js
+++ step5/step5-3/controller.js
@@ -2,6 +2,7 @@ const Controller = function (model, view) {
     this.model = model
     this.view = view
 }
+
 Controller.prototype = {
     showAll() {
         const countResult = {
@@ -11,42 +12,50 @@ Controller.prototype = {
         }
         this.view.showAll(countResult)
     },
+
     showEachData(status) {
         const countNumber = this.model.countData(status)
         const targetData = this.model.getMatchedData(status)
         this.view.showEachData(status, countNumber, targetData)
     },
+
     showData(type) {
         if (type === 'all') { return this.showAll() }
         if (!/^(todo|doing|done)$/.test(type)) throw Error('showOptionError')
         this.showEachData(type)
     },
+
     addData(name, tags) {
         const id = this.model.makeId()
         const changedData = this.model.addData(name, tags, id);
-        this.view.showAddResult(changedData);
+        this.view.showResult('addData', changedData);
         this.showFinalResult();
     },
+
     deleteData(id) {
         const changedData = this.model.deleteData(id)
-        this.view.showDeleteResult(changedData);
+        this.view.showResult('deleteData', changedData);
         this.showFinalResult();
     },
+
     updateData(id, status) {
         if (!/^(todo|doing|done)$/.test(status)) throw Error('updateOptionError')
         const changedData = this.model.updateData(id, status);
         setTimeout(() => {
-            this.view.showUpdateResult(changedData);
+            this.view.showResult('updateData', changedData);
             this.showFinalResult();
         }, 3000);
     },
+
     showFinalResult() {
         setTimeout(() => { this.showAll() }, 1000);
     },
+    
     undo() {
         const data = this.model.undoData();
         this.view.showUndoRedoResult(data);
     },
+
     redo() {
         const data = this.model.redoData();
         this.view.showUndoRedoResult(data);
--- step5/step5-3/view.js
+++ step5/step5-3/view.js
@@ -1,33 +1,34 @@
 const View = function (fontColor) {
     this.fontColor = fontColor;
 }
+
 View.prototype = {
     showAll(countResult) {
         console.log(this.fontColor, '현재상태 : ' + Object.entries(countResult).map(([key, value]) => `${key}: ${value}개`).join(', '))
     },
+
     showEachData(status, countNumber, targetData) {
         const str = targetData.map(el => `'${el.name}, ${el.id}번'`).join(', ')
         console.log(this.fontColor, `${status}리스트 : 총 ${countNumber}건 : ${str}`)
     },
+
     //changedData = object
-    showAddResult(changedData) {
-        const { name, id } = changedData;
-        console.log(this.fontColor, `${name} 1개가 추가되었습니다. (id : ${id})`)
+    showResult(keyCommand, changedData) {
+        const { name, status, id } = changedData;
+        const msgMap = {
+            addData: `${name} 1개가 추가되었습니다. (id : ${id})`,
+            deleteData: `${name} ${status}가 목록에서 삭제되었습니다.`,
+            updateData: `${name}이(가) ${status}으로 상태가 변경되었습니다.`
+        }
+        console.log(this.fontColor, msgMap[keyCommand])
     },
-    showDeleteResult(changedData) {
-        const { name, status } = changedData;
-        console.log(this.fontColor, `${name} ${status}가 목록에서 삭제되었습니다.`)
-    },
-    showUpdateResult(changedData) {
-        const { name, status } = changedData;
-        console.log(this.fontColor, `${name}이(가) ${status}으로 상태가 변경되었습니다.`)
-    },
+
     showUndoRedoResult(data) {
         const { keyCommand, newData } = data;
-        const { id, name, status } = newData;
+        const { name, status, id } = newData;
         const msgMap = {
-            deleteData: `${id, name}가 삭제되었습니다.`,
             addData: `${id, name}가 삭제에서 ${status}로 변경되었습니다.`,
+            deleteData: `${id, name}가 삭제되었습니다.`,
             updateData: `${id, name}가 ${status}로 상태가 변경되었습니다.`
         }
         console.log(this.fontColor, msgMap[keyCommand])
