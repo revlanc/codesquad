4dd349f7414db3aeb83e5a55823d81797616bc76
allen <revenoir7@gmail.com>
2019-05-01 16:14:09 +0900

    feat: arrayParser.js 파일 생성

---
+++ step6/step6-1/arrayParser.js
@@ -0,0 +1,16 @@
+const str = "[123, [22, [55], 33], 44, [66], 77]";
+
+class ArrayParser {
+    constructor () {
+
+    }
+
+    getParsedStr() {
+
+    }
+}
+
+const arrayParser = new ArrayParser();
+
+const result = arrayParser.getParsedStr(str);
+console.log(result);
